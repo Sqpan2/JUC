e00b823677e6d29ffbae3ba3cc49acd4a80e6e0d
shiqi.pan2 <shiqi.pan2@zatech.com>
2020-12-29 19:47:06 +0800

    delete .idea

--- src/main/java/com/mashibing/Tets.java
+++ src/main/java/com/mashibing/Tets.java
@@ -1,10 +1,15 @@
 package com.mashibing;
 
+import lombok.Data;
+import org.apache.commons.beanutils.BeanUtils;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class Tets {
     public static void main(String[] args) {
-        ConcurrentHashMap map = new ConcurrentHashMap();
-        map.put("1", "msb");
+
     }
 }
